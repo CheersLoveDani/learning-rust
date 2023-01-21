650a20028b90a091a990dac218c30e2d8f12e926
Dani Fullerton <kingme441@gmail.com>
2023-01-21 13:33:23 +0000

    temp converter

--- projects/temp_converter/src/main.rs
+++ projects/temp_converter/src/main.rs
@@ -1,13 +1,28 @@
 use std::io;
 
 fn main() {
-    let mut temperature: String = String::new();
+    loop {
+        let mut temperature: String = String::new();
+        println!("Input your temp in C as a number");
 
-    println!("Input your temp in C");
+        io::stdin()
+            .read_line(&mut temperature)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut temperature)
-        .expect("Failed to read line");
+        let temperature: u32 = match temperature.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("{}", temperature);
+        let temperature: f64 = convert_celsius_to_fahrenheit(temperature);
+
+        println!("In fahrenheit this is: {}", temperature);
+
+        break;
+    }
+}
+
+fn convert_celsius_to_fahrenheit(temperature: u32) -> f64 {
+    let tempf = temperature as f64;
+    ((tempf * 9.0) / 5.0) + 32.0
 }
